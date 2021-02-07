775007f6b5dbc67d817b80ff424ea5024e0b9f81
sajmd <sajidjava90@gmail.com>
2021-02-07 23:54:14 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,13 +1,14 @@
 import os
+from datetime import datetime
 from flask import Flask, redirect
 
 app = Flask(__name__)
 messages = []
 
-
 def add_messages(username, message):
     """Add messages to the 'messages' list"""
-    messages.append("{}:{}".format(username, message))
+    now = datetime.now().strftime("%H:%M:%S")
+    messages.append("({}) {}: {}".format(now,username, message))
 
 def get_all_messages():
     """Get all of the messages and seperate them with a 'br'"""
